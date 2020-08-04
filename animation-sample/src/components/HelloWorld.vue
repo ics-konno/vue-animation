6faa0f538814ef2c08d3c21c416d3cc50aca0c77
ics-konno <konno@ics-web.jp>
2020-08-04 17:16:10 +0900

    名前付きの異なるアニメーションを作成

--- animation-sample/src/components/HelloWorld.vue
+++ animation-sample/src/components/HelloWorld.vue
@@ -1,8 +1,11 @@
 <template>
   <div class="hello">
     <button @click="toggleShow">toggle</button>
-    <transition>
-      <p v-show="isShown">Hello World</p>
+    <transition name="one">
+      <p v-show="isShown">アニメーション１</p>
+    </transition>
+    <transition name="two">
+      <p v-show="isShown">アニメーション２</p>
     </transition>
   </div>
 </template>
@@ -25,16 +28,30 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.v-enter-active {
+.one-enter-active {
   transition: opacity 0.5s;
 }
-.v-leave-active {
+.one-leave-active {
   transition: all 1.5s;
 }
-.v-enter  /* .fade-leave-active below version 2.1.8 */ {
+.one-enter  /* .fade-leave-active below version 2.1.8 */ {
   opacity: 0;
 }
-.v-leave-to {
+.one-leave-to {
+  opacity: 0;
+  transform: translateX(20px);
+}
+
+.two-enter-active {
+  transition: opacity 1.5s;
+}
+.two-leave-active {
+  transition: all 2.5s;
+}
+.two-enter  /* .fade-leave-active below version 2.1.8 */ {
+  opacity: 0;
+}
+.two-leave-to {
   opacity: 0;
   transform: translateX(20px);
 }
